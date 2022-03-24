244eeddab0d1c85c681940eec87b56832ecd2683
Λ B H I N Λ V <ixd.abhi@gmail.com>
2022-03-24 07:40:22 +0530

    Second generation has been implemented

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+$('#book-now').click(function(){
+   var email = document.getElementById("get-email-id").value;
+	console.log(email );
+});
+
---
+++ send.php
@@ -0,0 +1,17 @@
+// 
+// $email_address = $_POST['email_address'];
+
+// $to = "ixd.abhi@gmail.com";
+// $subject = "Hurry you booked your device";
+// $body = "Hi there, Thanks for booking your device. We will keep you posted about our progress.";
+
+// mail($to,$subject,$body);
+
+// echo "Message sent";
+
+// 
+
+<?php
+mail('ixd.abhi@gmail.com','Test mail','The mail function is working!');
+echo 'Mail sent!';
+?>
